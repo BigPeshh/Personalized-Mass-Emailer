882cc7862117a5774822346f08bf6acfd0980c42
Ryan Peschka <48938077+BigPeshh@users.noreply.github.com>
2019-04-03 21:19:07 -0500

    Update SendEmail.py

--- SendEmail.py
+++ SendEmail.py
@@ -5,12 +5,13 @@ import smtplib
 #Sign into email account
 e = input(str("Email Address: "))
 p = input(str("Password: "))
+r = input(str("Recipient Email: "))
 server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
 server.login(e, p)
 
 #The Email 
 msg = "Hello, this is a test email."
 
-#Sending email to myself
-server.sendmail(e, e, msg)
+#Sending email to recipient
+server.sendmail(e, r, msg)
 server.quit()
